491806e2a0a1764888b55bb4cfa3443b9c441545
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-22 14:15:53 +0200

    Create django-based autnetication system - login, logout, redircts after login.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login as django_login, logout as django_logout
+#from django.contrib.auth.views import logout-then-login
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='login'),
+    #url(r'^login/$', views.user_login, name='login'),
+
+    url(r'^login/$', django_login, name='login'),
+    url(r'^logout/$', django_logout, name='logout'),
+    # url(r'^logout-then-login/$',
+    #     logout-then-login,
+    #     name='logout-then-login'),
+    url(r'^$', views.dashboard, name='dashboard')
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -99,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
 
 
 # Internationalization
