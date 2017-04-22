f412f3fa5c1800bca1d16931ee184b811c76a8d4
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-22 18:35:57 +0200

    Create user registration mechanism.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     url(r'^password-reset/done/$', auth_views. PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
+    url(r'^register/$', views.register, name='register')
 ]
