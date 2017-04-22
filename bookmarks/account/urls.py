a48b0e06b05b210c8bdb438eaef806ee070fe75f
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-22 14:30:48 +0200

    Implement custom views for password change.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login as django_login, logout as django_logout
+from django.contrib.auth import views as auth_views
 #from django.contrib.auth.views import logout-then-login
 from . import views
 
 urlpatterns = [
     #url(r'^login/$', views.user_login, name='login'),
 
-    url(r'^login/$', django_login, name='login'),
-    url(r'^logout/$', django_logout, name='logout'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     # url(r'^logout-then-login/$',
     #     logout-then-login,
     #     name='logout-then-login'),
-    url(r'^$', views.dashboard, name='dashboard')
+    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^password-change$', auth_views.password_change, name='password_change'),
+    url(r'^password-change-done$', auth_views.password_change_done, name='password_change_done')
 ]
