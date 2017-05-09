3b28233944a10b2bf0bd52054844dd3fb662e5c5
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-05-09 22:20:42 +0200

    Implement image like feature with aax requests.

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 @login_required
 def image_create(request):
@@ -33,4 +35,25 @@ def image_detail(request, id, slug):
     return render(request,
                 'images/image/detail.html',
                 {'section': 'images',
-                'image': image})                                
+                'image': image})
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+
+    return JsonResponse({'status': 'ko'})
