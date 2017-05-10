6a0b50ebde3bb67eeeeebdce8b6677ced0cead77
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-05-10 22:22:52 +0200

    Createing list and detail view for user profile.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -21,5 +21,8 @@ urlpatterns = [
     url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     url(r'^register/$', views.register, name='register'),
-    url(r'^edit/$', views.edit, name='edit')
+    url(r'^edit/$', views.edit, name='edit'),
+
+    url(r'^users/$', views.user_list, name='user_list'),
+    url(r'^users/(?P<username>[-\w]+)/$', views.user_detail, name='user_detail'),
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -151,3 +151,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
