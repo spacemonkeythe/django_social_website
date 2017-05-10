242148c58bf4fba8ea11a79971f2d2892120d764
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-05-10 21:11:21 +0200

    Create many-to-many relations with an intermediary model - Contact- between django model - User

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
@@ -9,3 +10,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(User, related_name='rel_from_set')
+    user_to = models.ForeignKey(User, related_name='rel_to_set')
+
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to)
