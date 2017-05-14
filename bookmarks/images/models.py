7de6a175befffa8abfd4afc43a9da711af6e488a
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-05-14 16:12:09 +0200

    Use signals for denormalizing counts.

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -17,6 +17,8 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
 
+    total_likes = models.PositiveIntegerField(db_index=True, default=0)
+
     def __str__(self):
         return self.title
 
