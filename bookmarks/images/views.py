c7114a5f4e95e2a4b72a4b1a6055e961b4545b29
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-05-14 21:18:53 +0200

    Create image ranking feature.

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -44,6 +44,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     total_views = r.incr('image:{}:views'.format(image.id))
 
+    r.zincrby('image_ranikng', image.id, 1)
+
     return render(request,
                 'images/image/detail.html',
                 {'section': 'images',
@@ -93,3 +95,16 @@ def image_list(request):
 
     return render(request, 'images/image/list.html',
                 {'section': images, 'images': images})
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+
+    return render(request,
+                'images/image/ranking.html',
+                {'section': 'images',
+                'most_viewed': most_viewed})
